c9ec32fa1393c203225e464ab3288b7c5e7efec5
farhdcse7 <fmfarhad23@gmail.com>
2025-02-22 21:16:20 +0600

    initial templating

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sapiente similique rem delectus, sint
+    accusantium nesciunt totam beatae. Porro sint ratione, sed aliquid nobis eius inventore labore possimus veritatis
+    fuga.</h1>
+
+</template>
+
+<style scoped></style>
