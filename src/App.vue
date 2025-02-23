44e1d2e0198a0ce3c95c4d1e36ab86625139e43c
farhdcse7 <fmfarhad23@gmail.com>
2025-02-23 21:56:34 +0600

    Todo App UI Design using Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,41 @@
 </script>
 
 <template>
-  
+
   <div class="container">
-    <h1 class="text-bg-danger">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sapiente similique rem delectus, sint
-    accusantium nesciunt totam beatae. Porro sint ratione, sed aliquid nobis eius inventore labore possimus veritatis
-    fuga.</h1>
+    <div class="row my-5">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center">Task List</h4>
+
+            <div class="row">
+              <form action="">
+                <div class="col-md-12">
+                  <label for="" class="form-label">Add New Task</label>
+                  <input type="text" class="form-control">
+                </div>
+                <div class="mt-3">
+                  <input type="button" class="btn btn-success" value="Add Task">
+                </div>
+              </form>
+            </div>
+
+          </div>
+
+          <div class="card-body">
+            <h4 class="text-danger">Tasks</h4>
+            <ul class="list-group">
+              <li class="list-group-item d-flex justify-content-between align-items-center">List Item
+                <a href="" class="btn"><i class="fa-solid fa-xmark"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
- 
+
 </template>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
-const path=require('path');
+// const path=require('path');
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
